0a11fb3344187f7909709c5e29bed80fbaa8b0c8
ahmednour005 <ahmednoureldeen001@gmail.com>
2019-12-19 02:05:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavComponent from './Pages/Navbar';
+import Cart from './Pages/cart'
+import Product from './Pages/product'
+import productDetails from './Pages/productDetails'
+import Home from './Pages/home' 
+import NotFound from './Pages/nofound'
+import {BrowserRouter ,Switch,Route} from 'react-router-dom'
+import store from './Api/store'
+import {Provider} from 'react-redux'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <BrowserRouter>
+      <NavComponent />
+      <Switch>
+        <Route exact path="/ReactEcommerce" component={Home} />
+        <Route exact path="/product" component={Product} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/product/:id" component={productDetails} />
+        <Route  component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+    </Provider>
   );
 }
 

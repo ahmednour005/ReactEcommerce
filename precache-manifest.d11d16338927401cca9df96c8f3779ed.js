93941e5467b3dba6d67ddbd55dced3417f787cb5
ahmednour005 <ahmednoureldeen001@gmail.com>
2019-12-19 02:15:14 +0200

    Updates

---
+++ precache-manifest.d11d16338927401cca9df96c8f3779ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3b1be75d5d1c46e2e5810758cf24ac",
+    "url": "/ReactEcommerce/index.html"
+  },
+  {
+    "revision": "eef21e0e16ce86179682",
+    "url": "/ReactEcommerce/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c4947f033d8caafec774",
+    "url": "/ReactEcommerce/static/css/main.7f4d585e.chunk.css"
+  },
+  {
+    "revision": "eef21e0e16ce86179682",
+    "url": "/ReactEcommerce/static/js/2.ad866052.chunk.js"
+  },
+  {
+    "revision": "704a91b005260117b5b4bae209198db9",
+    "url": "/ReactEcommerce/static/js/2.ad866052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4947f033d8caafec774",
+    "url": "/ReactEcommerce/static/js/main.64953dca.chunk.js"
+  },
+  {
+    "revision": "2c6b83e81f0b9754b21d",
+    "url": "/ReactEcommerce/static/js/runtime-main.f582e885.js"
+  }
+]);
